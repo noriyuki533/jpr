bc26b64ddf446c6f4680236127d7edf0c2f8bdbb
hso <hso@suou.waseda.jp>
2019-06-26 10:24:18 +0900

    r11_2

---
+++ SampleInput3.java
@@ -0,0 +1,15 @@
+public class SampleInput3 {
+    public static void main(String[] args) {
+	System.out.print("Input int > ");
+	int a = Input.intNum();
+	System.out.println("a : " + a);
+	
+	System.out.print("Input double > ");
+	double b = Input.doubleNum();
+	System.out.println("b : " + b);
+	
+	System.out.print("Input String > ");
+	String s = Input.string();
+	System.out.println("s : " + s);
+    }
+}
